252bf2a517dd4331b6a51762958c1b453214f8a9
Angelique99 <angelique.touraine@gmail.com>
2026-04-07 15:59:10 +0200

    Logements terminés

--- Kasa/src/components/Rating.jsx
+++ Kasa/src/components/Rating.jsx
@@ -1,12 +1,18 @@
+import starActive from "../assets/star-active.png";
+import starInactive from "../assets/star-inactive.png";
+
 function Rating({ rating }) {
   const stars = [1, 2, 3, 4, 5];
 
   return (
     <div className="rating">
       {stars.map((star) => (
-        <span key={star}>
-          {star <= rating ? "⭐" : "☆"}
-        </span>
+        <img
+          key={star}
+          src={star <= rating ? starActive : starInactive}
+          alt="star"
+          className="star"
+        />
       ))}
     </div>
   );
